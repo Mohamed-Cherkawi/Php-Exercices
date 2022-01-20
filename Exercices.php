fa25b37e2fd137837762a7affd4a6a59f17c6cef
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-01-20 10:26:29 +0100

    L'ajout de L'exercice 4

--- Exercices.php
+++ Exercices.php
@@ -1,14 +1,14 @@
 <?php
 //     // Exercice 2
-//  echo "Hello Youcode <br>" ;
+ echo "Hello Youcode <br>" ;
 
-//     // Exercice 3
+    // Exercice 3
 
     $monNom = " Cherkaoui <br>" ;
-//    $monAge = 19 ; 
-//    $monPrenom = "Mohamed" ;
-//    echo "$monNom  $monPrenom  $monAge <br>" ; 
-//    echo '$monNom  $monPrenom  $monAge <br>' ; 
+   $monAge = 19 ; 
+   $monPrenom = "Mohamed" ;
+   echo "$monNom  $monPrenom  $monAge <br>" ; 
+   echo '$monNom  $monPrenom  $monAge <br>' ; 
 
    /*La difference ce que la deuxime ne prend pas en considération
    ces variables , il les considére comme une chaine de caracthère pa plus 
@@ -26,12 +26,26 @@
     $str2 = <<<'MYTEXT'
     This is a
     demo message
-    with heredoc $monNom
+    with heredoc $monNom 
     MYTEXT;
     
-    echo $str2;
+    echo $str2 . "<br>";
 
     // L'avantage d'utiliser HEREDOC ou bien NOWDOC c'est que on a pas besoin d'écrire chque fois le signe de passer \ , cette methode ne permet d'écrire directement sans problèmes.
  //   Heredoc strings are like double-quoted strings without escaping.
  //  Nowdoc strings are like single-quoted strings without escaping.
+    $myBool = false; 
+
+    var_dump(+$myBool);
+    // Converting a boolean to an int
+    $number = 11;
+
+    $string = strval($number);
+
+    echo $string ;
+
+    echo "<br>" ;
+
+    var_dump($string);
+        // Converting an int to a string
 
